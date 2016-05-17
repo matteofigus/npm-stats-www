dbb7a751163b98bbceb2af01ff9877f2d10da6e2
Matteo Figus <matteofigus@gmail.com>
2016-05-17 22:34:15 +0100

    Added recently updated modules on home page

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,44 @@
+var npmStats = require('npm-stats');
+var _ = require('lodash');
+
+var from = function(){
+  var d = new Date();
+  d.setHours(d.getHours() - 30);
+  return d;
+};
+
+var to = function(){
+  var d = new Date();
+  d.setHours(d.getHours() - 24);
+  return d;
+};
+
+var registry = npmStats();
+
+var getUpdated = function(callback){
+  registry.listByDate({ since: from(), until: to() }, function(err, r){
+    if(err){ return callback(err); }
+
+    var result = _(r)
+      .chain()
+      .map(function(item){
+        return item.name;
+      })
+      .uniq()
+      .value();
+
+    callback(null, result);
+  });
+};
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Npm-stats', username: '', repository: '' });
+
+  getUpdated(function(err, updatedModules){
+    res.render('index', {
+      title: 'Npm-stats',
+      username: '',
+      repository: '',
+      updatedModules: updatedModules || []
+    });
+  });
 };
